f965b4444bd194194558672e9331d58c6231dd5a
Ryan Doyle <ryandoyle@hey.com>
2022-05-20 12:10:25 -0700

    add roles to signup

--- web/src/pages/SignupPage/SignupPage.tsx
+++ web/src/pages/SignupPage/SignupPage.tsx
@@ -1,5 +1,5 @@
 import { Link, navigate, routes } from '@redwoodjs/router'
-import { useRef } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import {
   Form,
   Label,
@@ -11,11 +11,14 @@ import {
 import { useAuth } from '@redwoodjs/auth'
 import { MetaTags } from '@redwoodjs/web'
 import { toast, Toaster } from '@redwoodjs/web/toast'
-import { useEffect, Fragment } from 'react'
 import { Tab } from '@headlessui/react'
+
+import StudentSVG from 'src/components/svg/StudentSVG/StudentSVG'
+import TeacherSVG from 'src/components/svg/TeacherSVG/TeacherSVG'
 
 const SignupPage = () => {
   const { isAuthenticated, signUp } = useAuth()
+  const [accountRole, setAccountRole] = useState('student')
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -30,7 +33,8 @@ const SignupPage = () => {
   }, [])
 
   const onSubmit = async (data) => {
-    const response = await signUp({ ...data, role: 'test' })
+    window.confirm(`Are you sure you want to create ${accountRole} account?`)
+    const response = await signUp({ ...data, role: accountRole })
 
     if (response.message) {
       toast(response.message)
@@ -42,6 +46,18 @@ const SignupPage = () => {
     }
   }
 
+  const handleRoleChange = (index) => {
+    if (index === 1) {
+      setAccountRole('teacher')
+    } else {
+      setAccountRole('student')
+    }
+    console.log(accountRole)
+  }
+
+  const isSelected = 'w-1/2 bg-indigo-500 rounded-xl text-white'
+  const notSelected = 'w-1/2 rounded-xl'
+
   return (
     <>
       <MetaTags title="Signup" />
@@ -51,20 +67,45 @@ const SignupPage = () => {
         <div className="rw-scaffold rw-login-container">
           <div className="rw-segment">
             <header className="rw-segment-header">
-              <h2 className="rw-heading rw-heading-secondary">Signup</h2>
+              <h2 className="rw-heading rw-heading-secondary">Sign Up</h2>
             </header>
-            <Tab.Group defaultIndex={1}>
-              <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
-                <Tab>Student</Tab>
-                <Tab>Teacher</Tab>
-              </Tab.List>
-              <Tab.Panels>
-                <Tab.Panel>Content 1</Tab.Panel>
-                <Tab.Panel>Content 2</Tab.Panel>
-              </Tab.Panels>
-            </Tab.Group>
-
             <div className="rw-segment-main">
+              <div className="w-full">
+                <Tab.Group
+                  defaultIndex={1}
+                  onChange={(index) => {
+                    handleRoleChange(index)
+                  }}
+                >
+                  <div className="text-center w-full pb-4 text-indigo-500 font-bold">
+                    Create account as a:
+                  </div>
+                  <Tab.List className="m-auto w-48 h-8 flex space-x-1 rounded-xl p-1 bg-indigo-100">
+                    <Tab
+                      className={({ selected }) =>
+                        selected ? isSelected : notSelected
+                      }
+                    >
+                      Student
+                    </Tab>
+                    <Tab
+                      className={({ selected }) =>
+                        selected ? isSelected : notSelected
+                      }
+                    >
+                      Teacher
+                    </Tab>
+                  </Tab.List>
+                  <Tab.Panels>
+                    <Tab.Panel className="flex justify-center">
+                      <StudentSVG width={200} height={200} />
+                    </Tab.Panel>
+                    <Tab.Panel className="flex justify-center">
+                      <TeacherSVG width={200} height={200} />
+                    </Tab.Panel>
+                  </Tab.Panels>
+                </Tab.Group>
+              </div>
               <div className="rw-form-wrapper">
                 <Form onSubmit={onSubmit} className="rw-form-wrapper">
                   <Label
