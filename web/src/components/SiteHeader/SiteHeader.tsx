5b3903c6a7e2626a1a0df6db510ff84763ae29ec
Ryan Doyle <ryandoyle@hey.com>
2022-05-24 15:04:16 -0700

    adjust header size

--- web/src/components/SiteHeader/SiteHeader.tsx
+++ web/src/components/SiteHeader/SiteHeader.tsx
@@ -27,14 +27,14 @@ const SiteHeader = () => {
   }
 
   return (
-    <header className="h-24 w-full py-4 flex justify-between pl-4 pr-8">
+    <header className="h-20 w-full py-4 flex justify-between pl-4 pr-8">
       <button
-        className="h-16 my-auto flex"
+        className="h-12 my-auto flex"
         onClick={() => {
           navigateHome()
         }}
       >
-        <div className="h-16 w-16 my-auto">
+        <div className="h-12 w-12 my-auto">
           <CoinSVG width={40} height={40}></CoinSVG>
         </div>
         <span className="my-auto ml-6 text-4xl font-display">Class Kudos</span>
