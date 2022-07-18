0602aa6732b6a5d020f599657727fb0167eca034
Ryan Doyle <ryandoyle@hey.com>
2022-07-18 11:13:40 -0700

    add Toaster to whole app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 import { AuthProvider } from '@redwoodjs/auth'
-
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+import { Toaster } from '@redwoodjs/web/toast'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
@@ -14,6 +14,7 @@ const App = () => (
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
       <AuthProvider type="dbAuth">
         <RedwoodApolloProvider>
+          <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
           <Routes />
         </RedwoodApolloProvider>
       </AuthProvider>
